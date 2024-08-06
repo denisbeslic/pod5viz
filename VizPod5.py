b44c6007bfb8631eb48f22cd5efd348601da393a
denisbeslic <denis.beslic@outlook.com>
2024-08-06 11:37:08 +0200

    Add function to plot txt file of reads

--- VizPod5.py
+++ VizPod5.py
@@ -63,14 +63,22 @@ def plot_multiple_reads(pod5_input, num_reads=5, plot_out="all-example"):
         fig.write_html(f"{plot_out}.html")
 
 
-def plot_single_read(pod5_input, selected_read_id, plot_out="example"):
+def plot_single_read(pod5_input, selected_read_id, outdir, start_idx=0, end_idx=None):
+    if selected_read_id.startswith("@"):
+        selected_read_id = selected_read_id[1:]
+    
     with p5.Reader(pod5_input) as reader:
         read = next(reader.reads([selected_read_id]))
         sample_rate = read.run_info.sample_rate
+        offset = read.calibration.offset
+        scale = read.calibration.scale
         signal = read.signal
+        pA_signal = list(scale * (signal + offset))
+        signal = pA_signal
         time = np.arange(len(signal)) / sample_rate
+        timepoints = np.arange(len(signal))
         fig = go.Figure()
-        fig.add_trace(go.Scatter(x=time, y=signal))
+        fig.add_trace(go.Scatter(x=timepoints, y=signal))
         fig.update_layout(xaxis=dict(rangeslider=dict(visible=True)))
         fig.update_layout(
             title=f"Signal for pod5 file {pod5_input}",
@@ -83,10 +91,12 @@ def plot_single_read(pod5_input, selected_read_id, plot_out="example"):
             legend_title_font_color="green",
         )
 
-        fig.write_html(f"{plot_out}.html")
-
         plt.figure(figsize=(12,3))
-        plt.plot(time[401:701], signal[401:701])
+        plt.plot(timepoints, signal)
+
+        plot_out = os.path.join(outdir, selected_read_id)
+
+        fig.write_html(f"{plot_out}.html")
         plt.savefig(f"{plot_out}.jpg")
         plt.savefig(f"{plot_out}.svg")
 
@@ -103,31 +113,43 @@ def read_stats(reads, outdir):
     plt.savefig(os.path.join(outdir, "seaborn_plot.jpg"), dpi=300)
     return reads_l
 
-def signal_stats(reads, pod5):
+
+def get_signal(read_record, outdir, plot=True):
+    sample_rate = read_record.run_info.sample_rate
+    name = str(read_record.read_id)
+    offset = read_record.calibration.offset
+    scale = read_record.calibration.scale
+    signal = read_record.signal
+    pA_signal = list(scale * (signal + offset))
+    time = np.arange(len(signal)) / sample_rate
+    if plot:
+        export_signal(time, pA_signal, name, outdir)
+    return time, signal, name
+
+def signal_stats(reads, pod5, outdir):
     read_names = [i[1] for i in reads]
     logger.info(f" Total number of reads: {len(read_names)}")
     with p5.Reader(pod5) as reader:
-        for read_record in reader.reads(selection=read_names):
-        #for read_name in read_names:
-            print(read_record)
-            #read = next(reader.reads([read_name]))
-            sample_rate = read_record.run_info.sample_rate
-            signal = read_record.signal
-            time = np.arange(len(signal)) / sample_rate
-            print(signal)
-            exit()
-            
-        exit()
-    
-    for read_name in read_names:
-        with p5.Reader(pod5) as reader:
-            exit()
-    #print(read_names)
-    # TODO get mean signal length, possible drops
-    exit()
+        time, signal, name = zip(*[get_signal(read_record, outdir) for read_record in reader.reads(selection=read_names)])
 
-def export_signals(reads, pod5):
-    exit()
+def read_ids_from_file(reads_txt):
+    """Read read IDs from a text file."""
+    with open(reads_txt, 'r') as file:
+        read_ids = [line.strip() for line in file]
+    return read_ids
+
+
+def export_signal(time, pA_signal, name, outdir):
+    plot_name = os.path.join(outdir, name)
+    pA_signal = pA_signal[0:3000]
+    time = time[0:3000]
+    plt.figure(figsize=(18,3))
+    plt.plot(time, pA_signal)
+    plt.title(name)
+    plt.gca().xaxis.grid(True)
+    plt.gca().yaxis.grid(True)
+    plt.savefig(f"{plot_name}.png")
+    plt.close()
 
 
 @click.command()
@@ -147,6 +169,19 @@ def export_signals(reads, pod5):
     help = "Path to fastq file",
 )
 @click.option(
+    "--read",
+    type=str,
+    help = "Single read you want to plot",
+    default=None,
+)
+@click.option(
+    "--reads-txt",
+    type=str,
+    help = "Path to reads txt",
+    default=None,
+)
+
+@click.option(
     "--outdir",
     type=click.Path(
         file_okay=False, dir_okay=True, path_type=pathlib.Path
@@ -154,27 +189,34 @@ def export_signals(reads, pod5):
     help = "Path to output directory",
     default = "results/",
 )
-def main(pod5, fastq, outdir):
+def main(pod5, fastq, read, reads_txt, outdir):
     logger.info("pod5viz")
     logger.info(f" pod5 = {pod5}")
     logger.info(f" fastq = {fastq}")
+    logger.info(f" read = {read}")
+    logger.info(f" reads_txt = {reads_txt}")
     logger.info(f" outdir = {outdir}")
     
     os.makedirs(outdir, exist_ok=True)  
 
+    if read is not None:
+        plot_single_read(pod5, read, outdir)
+
+    if reads_txt is not None:
+        print(reads_txt)
+        # Extract read IDs from the text file
+        read_ids = read_ids_from_file(reads_txt)
+
+        # Plot each read ID
+        for read_id in read_ids:
+            plot_single_read(pod5, read_id, outdir)
+        exit()
+        
+
     reads = read_fasta(fastq)
     reads = read_stats(reads, outdir)
 
-    signal_stats(reads, pod5)
-
-    # TODO export pictures of signals
-
-    exit()
-
-    plot_multiple_reads(args.input, num_reads=5)
-    # TODO: Print additional information on graph f.e. total length, basecalled information
-    #selected_read_id = "76c01f6e-06eb-416f-88d9-eb6c7803afb8"
-    #plot_single_read(args.input, selected_read_id)
+    signal_stats(reads, pod5, outdir)
 
 
 if __name__ == "__main__":
